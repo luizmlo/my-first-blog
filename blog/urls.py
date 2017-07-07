85d16567421b61407d5ac56a6266358279be8954
Luiz Felipe Melo <luizf.mlobrc@gmail.com>
2017-07-07 01:34:01 -0300

    Added the BIG UPDATE Ver. 1

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from blog import views
 
 urlpatterns = [
     url(r'^$', views.post_list),
+    url(r'1', views.rid_1),
+    url(r'tunacan', views.rid_2),
+    url(r'macaroun', views.rid_3),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,18 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def rid_1(request):
+    return render(request, 'blog/rid_1.html')
+
+def rid_2(request):
+    return render(request, 'blog/rid_2.html')
+
+def rid_3(request):
+    return render(request, 'blog/rid_3.html')
+
+def rid_4(request):
+    return render(request, 'blog/rid_4.html')
+
+def rid_5(request):
+    return render(request, 'blog/rid_5.html')
